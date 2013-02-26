569a1ff7ce55088a8b0abcf92a0e02ce71873478
Martin Nguyen <martin@vinova.sg>
2013-02-26 07:26:27 +0700

    add some functions for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,33 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update_attributes(params[:item])
+      redirect_to items_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+
+  end
 end
